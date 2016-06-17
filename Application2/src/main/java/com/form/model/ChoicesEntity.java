53f34223ce64e00b4237f77de70892575cc23b54
satocc <hikaru.sato@casleyconsulting.co.jp>
2016-06-17 16:05:01 +0900

    諸々

--- Application2/src/main/java/com/form/model/ChoicesEntity.java
+++ Application2/src/main/java/com/form/model/ChoicesEntity.java
@@ -32,7 +32,7 @@ public class ChoicesEntity {
 		public int getQuestion_id(){
 			return question_id;
 		}
-		public void setQestion_id(int question_id){
+		public void setQuestion_id(int question_id){
 			this.question_id = question_id;
 		}
 		public int getAnswer_id(){
