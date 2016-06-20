407094c2de0374b1ac8e4f78e3c568c1cb3d2f1d
ann7o <anna.shinozaki@casleyconsulting.co.jp>
2016-06-20 18:49:32 +0900

    解答結果画面など
    解答結果画面
    ResultEntity 新規作成
    LoginController 修正

---
+++ Application2/src/main/java/com/form/model/ResultEntity.java
@@ -0,0 +1,57 @@
+package com.form.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+public class ResultEntity {
+
+	@Id
+	private int question_id;
+	@Column
+	private  String question;
+	@Column
+	private String MaruBatsu;
+	@Column
+	private String select_answer;
+	@Column
+	private String answer;
+	@Column
+	private String commentary;
+
+	public int getQuestion_id() {
+		return question_id;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public String getMaruBatsu() {
+		return MaruBatsu;
+	}
+	public String getSelect_answer(){
+		return select_answer;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public String getCommentary() {
+		return commentary;
+	}
+	public void setQuestion_id(int question_id) {
+		this.question_id = question_id;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public void setMaruBatsu(String maruBatsu) {
+		MaruBatsu = maruBatsu;
+	}
+	public void setSelect_answer(String select_answer){
+		this.select_answer = select_answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public void setCommentary(String commentary) {
+		this.commentary = commentary;
+	}
+}
