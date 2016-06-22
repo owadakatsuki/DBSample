f28e75fb410f6de814960923171a07028d0c00b8
ann7o <anna.shinozaki@casleyconsulting.co.jp>
2016-06-22 16:03:19 +0900

    TestController削除

--- Application2/src/main/java/com/form/model/UserAnswer.java
+++ Application2/src/main/java/com/form/model/UserAnswer.java
@@ -2,15 +2,15 @@
  * 日付 : 2016.06.15
  * 作成者 : キム
  * ユーザ解答に関してのモデル
- * 
- * 
+ *
+ *
  * 6/16
- * 	error : Hibernate : ids for this class must be manually assigned before calling save() 
+ * 	error : Hibernate : ids for this class must be manually assigned before calling save()
  *  resolve : id attribute is not set. this MAY be due to the fact that the DB field is not set to auto increment.
- *  		  so, i add three sentences. 
+ *  		  so, i add three sentences.
  *  		  @GeneratedValue(strategy = GenerationType.IDENTITY)
  *            @Basic(optional = false)
- *            @Column(name = "id",unique=true, nullable = false)	
+ *            @Column(name = "id",unique=true, nullable = false)
  */
 package com.form.model;
 
@@ -25,13 +25,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "user_answer_tb")
 public class UserAnswer implements Serializable { //implements Serializable{
 	private static final long serialVersionUID = 3744346731479843943L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
@@ -41,43 +40,43 @@ public class UserAnswer implements Serializable { //implements Serializable{
 	@NotNull
 	@Column
 	private Integer 	user_id;		// ユーザID
-	
+
 	@Column
 	private Integer 	content_id;		// 問題ID
-	
+
 	@Column
 	private Integer 	question_id;	// 小問ID
-	
+
 	@Column
 	private Integer 	answer_id;		// 解答番号
-	
+
 	@Column
 	private String 		select_answer;	// 選択した値
-	
+
 	@Column
-	private Timestamp 	time;			// 提出した時間(input default current local time) 
-	
+	private Timestamp 	time;			// 提出した時間(input default current local time)
+
 	public Integer getId() { return id; }
 	public void setId( Integer id ) { this.id = id; }
-	
+
 	public Integer getUser_id() { return user_id; }
 	public void setUser_id( Integer user_id ) { this.user_id = user_id; }
-	
+
 	public Integer getContent_id() { return content_id; }
 	public void setContent_id( Integer content_id ) { this.content_id = content_id; }
-	
+
 	public Integer getQuestion_id() { return question_id; }
 	public void setQuestion_id( Integer question_id ) { this.question_id = question_id; }
-	
+
 	public Integer getAnswer_id() { return answer_id; }
 	public void setAnswer_id( Integer answer_id ) { this.answer_id = answer_id; }
-	
+
 	public String getSelect_answer() { return select_answer; }
 	public void setSelect_answer( String select_answer ) { this.select_answer = select_answer; }
-	
+
 	public Timestamp getTime() { return time; }
 	public void setTime( Timestamp time ) { this.time = time; }
-	
+
 	@Override
 	public String toString() {
 		return "UserAnswer [ユーザID=" + user_id + ", コンテントID=" + content_id + ", クエスチョンID=" + question_id
