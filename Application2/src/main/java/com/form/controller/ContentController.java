ef8025a3da68550f3189b707746e839394c95a3f
eito-takishita <eito.takishita@casleyconsulting.co.jp>
2016-06-22 10:48:06 +0900

    Login,content修正
    /menuに遷移する処理をLoginControllerに追加

--- Application2/src/main/java/com/form/controller/ContentController.java
+++ Application2/src/main/java/com/form/controller/ContentController.java
@@ -11,77 +11,91 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.form.model.Content;
 import com.form.model.ContentRepository;
-import com.form.model.User;
 
 @Controller
 public class ContentController {
 
     @Autowired
     ContentRepository contentRepository;
+
+    // contentメソッドの処理は、LoginControllerに移します。
     @RequestMapping("/menu")
     public String content(Locale locale, Model model) {
         System.out.println("[START] データベースに接続してデータを取得します。");
 
+        // 大問一覧取得
         List<Content> contentList = contentRepository.findAll();
 
-        for (Content content : contentList) {
-            System.out.println(content.getContent_id() + content.getContent_title());
-        }
+//        for (Content content : contentList) {
+//            System.out.println(content.getContent_id() + content.getContent_title());
+//        }
 
+/** 管理者権限でログインしたときに、ボタンが非表示になるか確認するための処理
+ *
         User user_info = new User();
         user_info.setRole("role");
         model.addAttribute("user_info", user_info);
+
+*/
+        // 大問一覧をセット
         model.addAttribute("contentList", contentList);
 
-        return "menu"; // メニュー画面に遷移
+        return "menu";
     }
 
     @RequestMapping(value="/remove")
     public String remove(@RequestParam("contentId") int contentID, Locale locale, Model model) {
         System.out.println("[START] 削除を行います。" + contentID);
 
+        // 問題の削除
         contentRepository.delete(contentID);
-        List<Content> contentList = contentRepository.findAll();
-
-        for (Content content : contentList) {
-            System.out.println(content.getContent_id() + content.getContent_title());
-        }
 
         System.out.println("[END] 削除しました。");
 
-        model.addAttribute("contentList", contentList);
-
-        return "menu"; // 削除後、メニュー画面のまま
-    }
-
-    @RequestMapping("/qustion")
-    public String question(Locale locale, Model model) {
-        System.out.println("[START] 解答画面に遷移します。");
-
         List<Content> contentList = contentRepository.findAll();
 
-        for (Content content : contentList) {
-            System.out.println(content.getContent_id() + content.getContent_title());
-        }
+        /** 管理者権限でログインしたときに、ボタンが非表示になるか確認するための処理
+         *
+                User user_info = new User();
+                user_info.setRole("role");
+                model.addAttribute("user_info", user_info);
 
+        */
+
+        // 削除後の大問一覧をセット
         model.addAttribute("contentList", contentList);
 
         return "menu";
     }
 
-    @RequestMapping("/make_form")
-    public String makeForm(Locale locale, Model model) {
-        System.out.println("[START] フォーム編集画面に遷移します。");
-
-        List<Content> contentList = contentRepository.findAll();
-
-        for (Content content : contentList) {
-            System.out.println(content.getContent_id() + content.getContent_title());
-        }
-
-        model.addAttribute("contentList", contentList);
-
-        return "menu";
-    }
+//    @RequestMapping("/qustion")
+//    public String question(Locale locale, Model model) {
+//        System.out.println("[START] 解答画面に遷移します。");
+//
+//        List<Content> contentList = contentRepository.findAll();
+//
+//        for (Content content : contentList) {
+//            System.out.println(content.getContent_id() + content.getContent_title());
+//        }
+//
+//        model.addAttribute("contentList", contentList);
+//
+//        return "menu";
+//    }
+//
+//    @RequestMapping("/make_form")
+//    public String makeForm(Locale locale, Model model) {
+//        System.out.println("[START] フォーム編集画面に遷移します。");
+//
+//        List<Content> contentList = contentRepository.findAll();
+//
+//        for (Content content : contentList) {
+//            System.out.println(content.getContent_id() + content.getContent_title());
+//        }
+//
+//        model.addAttribute("contentList", contentList);
+//
+//        return "menu";
+//    }
 
 }
